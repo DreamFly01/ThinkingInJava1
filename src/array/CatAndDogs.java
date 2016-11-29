5b8818d8c4f30dff22a8af0878fc4d6d20cef212
yuchang <378434999@qq.com>
2016-11-29 17:49:59 +0800

    Vetor 集合用例

---
+++ src/array/CatAndDogs.java
@@ -0,0 +1,36 @@
+package array;
+
+import java.util.Vector;
+
+/**
+ * Created by dell on 2016/11/29.
+ */
+public class CatAndDogs {
+    public static void main(String[] args){
+        Vector cats  = new Vector();
+        for (int i = 0; i < 7; i++) {
+            cats.addElement(new Cat(i));
+        }
+        for (int i = 0; i < cats.size(); i++) {
+            ((Cat)cats.elementAt(i)).print();
+        }
+    }
+}
+class Cat{
+    private int catNumber;
+    Cat(int i ){
+        catNumber = i;
+    }
+    void print(){
+        System.out.println("Cat #"+catNumber);
+    }
+}
+class Dog{
+    private int dogNumber;
+    Dog(int i ){
+        dogNumber = i;
+    }
+    void print(){
+        System.out.println("Dog #"+dogNumber);
+    }
+}
